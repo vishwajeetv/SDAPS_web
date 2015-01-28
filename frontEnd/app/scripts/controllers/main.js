e8cac4f4d7682c55d6bcca575ed48ec524ea69de
aparna <aparna.patil@connvertex.com>
2015-01-28 18:43:25 +0530

    Merge branch 'master' of /home/aparna/SDAPS_web with conflicts.

--- frontEnd/app/scripts/controllers/main.js
+++ frontEnd/app/scripts/controllers/main.js
@@ -14,66 +14,41 @@ angular.module('sdapsApp')
       'AngularJS',
       'Karma'
     ];
-        $scope.onFileSelect = function($files) {
 
-            if (!sessionStorage.authenticated)
-            {
-                $location.path('/');
-            }
-
-            console.log($files); // undefined
-            //$files: an array of files selected, each file has name, size, and type.
-            for (var i = 0; i < $files.length; i++) {
-                var file = $files[i];
-                $scope.upload = $upload.upload({
-                    url: 'http://192.168.2.232/sdaps/public/form/upload-form', //upload.php script, node.js route, or servlet url
-                    data: file,
-                    file: file
-                }).progress(function(evt) {
-                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-                }).success(function(data, status, headers, config) {
-                    // file is uploaded successfully
-                    console.log(data);
-                });
-            }
-        };
-
-
-        $scope.departments =  null;
-        $timeout(function () {
-            $scope.getDepartments();
-        }, 1);
-
-
-        $scope.generateCharts = function() {
+    $scope.generateCharts = function()
+        {
             var getReportsMethod = Restangular.all('index.php/form/generate-reports-from-db');
 
             getReportsMethod.post().then(function (response) {
 
                 console.log(response.body);
+
                 var report = response.body;
                 var totalReportGradeCount = report.total.gradeCount;
                 var totalCount = report.total.count;
 
-
                 $scope.chartConfig = {
 
-                    options: {
-                        chart: {
-                            type: 'bar'
+                    options: 
+                    {
+                        chart: 
+                        {
+                            type: 'line'
                         }
                     },
+
+                    useHighStocks: false,
                     title: {
-                        text: "good"
+                        text: "Good"
                     },
                     xAxis: {
                         categories: ['1-3 times', '3-6 times', 'more than 6 times']
                     },
                     yAxis: {
                         labels: {
-                            formatter: function () {
+                            formatter:function() {
                                 var pcnt = (this.value / totalCount) * 100;
-                                return Highcharts.numberFormat(pcnt, 0, ',') + '%';
+                                return Highcharts.numberFormat(pcnt,0,',') + '%';
                             }
                         }
                     },
@@ -101,16 +76,45 @@ angular.module('sdapsApp')
                         colorByPoint: true,
                         data: [
 
-                            ['excellent', totalReportGradeCount.excellent],
-                            ['good', totalReportGradeCount.good],
-                            ['satisfactory', totalReportGradeCount.satisfactory],
-                            ['unsatisfactory', totalReportGradeCount.unsatisfactory],
-                            ['mediocre', totalReportGradeCount.mediocre]
+                            ['times_1_3', totalReportGradeCount.times_1_3],
+                            ['3-6_times', totalReportGradeCount.times_3_6],
+                            ['6_more_times', totalReportGradeCount.times_6_more]
                         ]
                     }]
-                }
+                } 
             });
         };
+
+        $scope.onFileSelect = function($files) {
+
+            if (!sessionStorage.authenticated)
+            {
+                $location.path('/');
+            }
+
+            console.log($files); // undefined
+            //$files: an array of files selected, each file has name, size, and type.
+            for (var i = 0; i < $files.length; i++) {
+                var file = $files[i];
+                $scope.upload = $upload.upload({
+                    url: 'http://192.168.2.232/sdaps/public/form/upload-form', //upload.php script, node.js route, or servlet url
+                    data: file,
+                    file: file,
+                }).progress(function(evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log(data);
+                });
+            }
+        };
+
+
+        $scope.departments =  null;
+        $timeout(function () {
+            $scope.getDepartments();
+        }, 1);
+
 
         $scope.getDepartments = function()
         {
